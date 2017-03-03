aa9fd5c5975bd294d04e253c4976167c041469a2
Bhaumik Gadani <bhaumikgithub@gmail.com>
2017-03-03 17:34:35 +0530

    Add https in to allow action cable in secure connection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://pacific-ravine-49577.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://pacific-ravine-49577.herokuapp.com', /http:\/\/pacific-ravine-49577.herokuapp.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://pacific-ravine-49577.herokuapp.com', /http:\/\/pacific-ravine-49577.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true

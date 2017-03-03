509b72bcb4396ab1980638040a534013f30c846b
Bhaumik Gadani <bhaumikgithub@gmail.com>
2017-03-03 18:47:31 +0530

    Comment action_cable.mount_path in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  config.action_cable.mount_path = nil
+  # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://pacific-ravine-49577.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://pacific-ravine-49577.herokuapp.com', /http:\/\/pacific-ravine-49577.herokuapp.*/ ]
 
